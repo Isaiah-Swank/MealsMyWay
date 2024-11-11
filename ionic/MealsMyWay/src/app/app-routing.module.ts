eea6a10828e4afcd2a2f0ddeeb25323f4d6b07ce
Max Poole <mkp274@nau.edu>
2024-11-11 16:40:34 -0700

    added routes

--- ionic/MealsMyWay/src/app/app-routing.module.ts
+++ ionic/MealsMyWay/src/app/app-routing.module.ts
@@ -5,7 +5,28 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.Tab1PageModule)
+  },
+  {
+    path: 'calendar',
+    loadChildren: () => import('./calendar/calendar.module').then( m => m.Tab2PageModule)
+  },
+  {
+    path: 'pantry',
+    loadChildren: () => import('./pantry/pantry.module').then( m => m.PantryPageModule)
+  },
+  {
+    path: 'recipes',
+    loadChildren: () => import('./recipes/recipes.module').then( m => m.RecipesPageModule)
+  },
+  {
+    path: 'profile',
+    loadChildren: () => import('./profile/profile.module').then( m => m.Tab3PageModule)
   }
+  
 ];
 @NgModule({
   imports: [
--- ionic/MealsMyWay/src/app/tabs/tabs-routing.module.ts
+++ ionic/MealsMyWay/src/app/tabs/tabs-routing.module.ts
@@ -8,27 +8,35 @@ const routes: Routes = [
     component: TabsPage,
     children: [
       {
-        path: 'tab1',
-        loadChildren: () => import('../tab1/tab1.module').then(m => m.Tab1PageModule)
+        path: 'home',
+        loadChildren: () => import('../home/home.module').then(m => m.Tab1PageModule)
       },
       {
-        path: 'tab2',
-        loadChildren: () => import('../tab2/tab2.module').then(m => m.Tab2PageModule)
+        path: 'calendar',
+        loadChildren: () => import('../calendar/calendar.module').then(m => m.Tab2PageModule)
       },
       {
-        path: 'tab3',
-        loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
+        path: 'pantry',
+        loadChildren: () => import('../pantry/pantry.module').then(m => m.PantryPageModule)
+      },
+      {
+        path: 'recipes',
+        loadChildren: () => import('../recipes/recipes.module').then(m => m.RecipesPageModule)
+      },
+      {
+        path: 'profile',
+        loadChildren: () => import('../profile/profile.module').then(m => m.Tab3PageModule)
       },
       {
         path: '',
-        redirectTo: '/tabs/tab1',
+        redirectTo: '/tabs/home',
         pathMatch: 'full'
       }
     ]
   },
   {
     path: '',
-    redirectTo: '/tabs/tab1',
+    redirectTo: '/tabs/home', 
     pathMatch: 'full'
   }
 ];
