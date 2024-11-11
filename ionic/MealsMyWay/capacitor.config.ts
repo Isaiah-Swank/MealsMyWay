492a10046d568a2e16b7cfcecfd4a14b46d2608d
Max Poole <mkp274@nau.edu>
2024-11-11 14:53:08 -0700

    ionic setup

---
+++ ionic/MealsMyWay/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'MealsMyWay',
+  webDir: 'www'
+};
+
+export default config;
